3fb35ec0de77d1ace36b531047c50529ccc52e20
zhiyan <1533460130@qq.com>
2022-07-13 17:54:46 +0800

    放前端项目

--- Wei_blog/src/main/java/com/xhy/wblog/dao/FansDao.java
+++
@@ -1,7 +0,0 @@
-package com.xhy.wblog.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.xhy.wblog.entity.Fans;
-
-public interface FansDao extends BaseMapper<Fans> {
-}
--- Wei_blog/src/main/java/com/xhy/wblog/dao/TopicDao.java
+++
@@ -1,7 +0,0 @@
-package com.xhy.wblog.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.xhy.wblog.entity.Topic;
-
-public interface TopicDao extends BaseMapper<Topic> {
-}
